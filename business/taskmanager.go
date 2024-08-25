6cb6c96e227f133973775558538325b7515fa7e7
Yogesh.Upadhyay <yogeshh2021@gmail.com>
2024-08-25 18:15:07 +0530

    add task manager initializers

--- business/taskmanager.go
+++ business/taskmanager.go
@@ -1,11 +1,28 @@
 package business
 
-import "github.com/rs/zerolog/log"
+import (
+	"task-scheduler/entities"
 
-const allocateResourceFuncName = "AllocateResource"
+	"github.com/google/uuid"
+	"github.com/rs/zerolog/log"
+)
 
-func AllocateResource() error {
+const allocateResourceFuncName = "AllocateResourceToTask"
+
+func AllocateResourceToTask() error {
 	log.Debug().Msgf("%s: allocate resource to task", allocateResourceFuncName)
 	log.Debug().Msgf("%s: completed allocating resource to task", allocateResourceFuncName)
+	return nil
+}
+
+func InitializeTaskManager() error {
+	dataCenterId := uuid.NewString()
+
+	dc := entities.DataCenter{
+		DataCenterId: dataCenterId,
+		Location:     "ap-south-1",
+		Resources:    []entities.Resource{},
+	}
+
 	return nil
 }
